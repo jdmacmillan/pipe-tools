803de3ccbd7e7af11a7f36965c43e23c0bf626a2
JD MacMillan <jdmacmil@ncsu.edu>
2021-01-02 20:48:01 -0500

    initial commit

---
+++ js/design.js
@@ -0,0 +1,3 @@
+function handleDesign(event) {
+
+}
---
+++ js/utils.js
@@ -0,0 +1,39 @@
+function attemptToParse(row) {
+  return row.map(column => {
+    const number = parseFloat(column, 10);
+
+    if (Number.isNaN(number)) return column;
+    return number;
+  });
+}
+
+function getFiles() {
+  const fileInput = document.getElementById('files');
+  return fileInput.files;
+}
+
+// This won't need to change at all.
+function downloadWorkbook(workbook, workbookName) {
+  // Write the workbook that's been created into a buffer of content
+  workbook.xlsx.writeBuffer({ base64: true })
+    .then(binaryData => {
+      // Basically this will create a new, hidden link that will download this file, then click the link.
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+
+      // Create the actual URL from the buffer.
+      var url = window.URL.createObjectURL(new Blob([binaryData], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }));
+
+      a.href = url;
+      a.download = workbookName;
+
+      a.click();
+
+      // Cleanup
+      window.URL.revokeObjectURL(url);
+    })
+    .catch(function (error) {
+      console.error(error.message);
+    });
+}
